308fd6c6229e14aaced0fff67adb73e0495e646c
Ebenezer <agyekumebenezer2001@gmail.com>
2023-04-13 23:27:08 +0000

    created product details page

---
+++ gamkrib/src/components/productDetails/ProductDetails.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { GeneralNavbar } from "../navbar/Navbar";
+import { Footer } from "../footer/Footer";
+
+export const ProductDetails = () => {
+  return (
+    <>
+      <GeneralNavbar />
+      ProductDetails
+      <Footer />
+    </>
+  );
+};
