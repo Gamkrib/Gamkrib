86c1fcfe6fe0d132fc5ab55c120e3c59ff2d2762
Ebenezer <agyekumebenezer2001@gmail.com>
2023-05-10 13:00:39 +0000

    added syncfusion auth and sales chart

--- gamkrib/src/components/admin/landlord/pages/Sales.jsx
+++ gamkrib/src/components/admin/landlord/pages/Sales.jsx
@@ -1,5 +1,107 @@
 import React from "react";
+import {
+  StockChartComponent,
+  StockChartSeriesCollectionDirective,
+  StockChartSeriesDirective,
+  Inject,
+  DateTime,
+  Tooltip,
+  RangeTooltip,
+  Crosshair,
+  LineSeries,
+  SplineSeries,
+  CandleSeries,
+  HiloOpenCloseSeries,
+  HiloSeries,
+  RangeAreaSeries,
+  Trendlines,
+  IStockChartEventArgs,
+  ChartTheme,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  EmaIndicator,
+  RsiIndicator,
+  BollingerBands,
+  TmaIndicator,
+  MomentumIndicator,
+  SmaIndicator,
+  AtrIndicator,
+  AccumulationDistributionIndicator,
+  MacdIndicator,
+  StochasticIndicator,
+  Export,
+} from "@syncfusion/ej2-react-charts";
+
+import { aapl } from "../../data/DummyData";
+
+const SAMPLE_CSS = `
+ .control-fluid {
+     padding: 0px !important;
+ }
+     .charts {
+         align :center
+     }`;
 
 export const Sales = () => {
-  return <div>Sales</div>;
+  return (
+    <div className="control-pane">
+      <style>{SAMPLE_CSS}</style>
+      <div className="control-section">
+        <StockChartComponent
+          id="stockchartspline"
+          primaryXAxis={{
+            valueType: "DateTime",
+            majorGridLines: { width: 0 },
+            crosshairTooltip: { enable: true },
+          }}
+          primaryYAxis={{
+            lineStyle: { color: "transparent" },
+            majorTickLines: { color: "transparent", width: 0 },
+          }}
+          indicatorType={[]}
+          seriesType={[]}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true }}
+          crosshair={{ enable: true }}
+          title="Sales Chart"
+        >
+          <Inject
+            services={[
+              DateTime,
+              Tooltip,
+              RangeTooltip,
+              Crosshair,
+              LineSeries,
+              SplineSeries,
+              CandleSeries,
+              HiloOpenCloseSeries,
+              HiloSeries,
+              RangeAreaSeries,
+              Trendlines,
+              EmaIndicator,
+              RsiIndicator,
+              BollingerBands,
+              TmaIndicator,
+              MomentumIndicator,
+              SmaIndicator,
+              AtrIndicator,
+              Export,
+              AccumulationDistributionIndicator,
+              MacdIndicator,
+              StochasticIndicator,
+            ]}
+          />
+          <StockChartSeriesCollectionDirective>
+            <StockChartSeriesDirective
+              dataSource={aapl}
+              xName="x"
+              yName="high"
+              type="Spline"
+            ></StockChartSeriesDirective>
+          </StockChartSeriesCollectionDirective>
+        </StockChartComponent>
+      </div>
+    </div>
+  );
 };
--- gamkrib/src/index.js
+++ gamkrib/src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
+import { registerLicense } from "@syncfusion/ej2-base";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+registerLicense(
+  "ORg4AjUWIQA/Gnt2VFhiQlVPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9gSXtSdkVrWHhec31STmM="
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -14,4 +19,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
