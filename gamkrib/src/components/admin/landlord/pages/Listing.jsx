875da8869a65d38d75f94c7ed2a878d1e427f186
Ebenezer <agyekumebenezer2001@gmail.com>
2023-05-13 14:37:50 +0000

    changing the colors of tubes

--- gamkrib/src/components/admin/landlord/pages/Listing.jsx
+++ gamkrib/src/components/admin/landlord/pages/Listing.jsx
@@ -8,10 +8,16 @@ import styled from "styled-components";
 import { DashBoardContext } from "../../../../context/selectedPropertyContext/DashBoardContext";
 import { MdOutlineAddBox } from "react-icons/md";
 
-export const Listing = () => {
-  const { selectedList, setSelectedList } = useContext(DashBoardContext);
+import { AiOutlineCheckCircle } from "react-icons/ai";
+import { motion } from "framer-motion";
+import { colors } from "../../../../theme/colors";
 
-  console.log(selectedList);
+import "./minorStyles.css";
+
+export const Listing = () => {
+  const { selectedList, isActive, setSelectedList, setIsActive } =
+    useContext(DashBoardContext);
+
   return (
     <div style={{ backgroundColor: "#f3f4f6", marginTop: 0 }}>
       <DashboardContainer>
@@ -30,6 +36,45 @@ export const Listing = () => {
             New Listing
           </AddBtn>
         </FlexDiv>
+        <PropertyState>
+          <PropertyBtn
+            onClick={() => setIsActive("active")}
+            className={
+              isActive === "active" ? "isActiveColor" : "notActiveColor"
+            }
+            whileTap={{ scale: 0.9 }}
+          >
+            <AiOutlineCheckCircle size={20} />
+            <span>Active </span>
+          </PropertyBtn>
+          <PropertyBtn
+            className={
+              isActive === "active" ? "isActiveColor" : "notActiveColor"
+            }
+            whileTap={{ scale: 0.9 }}
+          >
+            <AiOutlineCheckCircle size={20} />
+            <span>Review </span>
+          </PropertyBtn>
+          <PropertyBtn
+            className={
+              isActive === "active" ? "isActiveColor" : "notActiveColor"
+            }
+            whileTap={{ scale: 0.9 }}
+          >
+            <AiOutlineCheckCircle size={20} />
+            <span>Declined </span>
+          </PropertyBtn>
+          <PropertyBtn
+            className={
+              isActive === "active" ? "isActiveColor" : "notActiveColor"
+            }
+            whileTap={{ scale: 0.9 }}
+          >
+            <AiOutlineCheckCircle size={20} />
+            <span>Draft </span>
+          </PropertyBtn>
+        </PropertyState>
       </DashboardContainer>
     </div>
   );
@@ -49,4 +94,29 @@ const AddBtn = styled(GreenBtn)`
 const FlexDiv = styled.div`
   display: flex;
   justify-content: space-between;
+`;
+
+const PropertyState = styled.div`
+  display: flex;
+  gap: 2rem;
+  margin-top: ${(p) => p.theme.space[1]};
+`;
+
+const PropertyBtn = styled(motion.div)`
+  width: ${(p) => p.theme.sizes[6]};
+
+  display: flex;
+  height: ${(p) => p.theme.sizes[4]};
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  border-radius: 30px;
+  cursor: pointer;
+
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */
 `;
--- gamkrib/src/context/selectedPropertyContext/DashBoardContext.js
+++ gamkrib/src/context/selectedPropertyContext/DashBoardContext.js
@@ -4,9 +4,12 @@ export const DashBoardContext = createContext();
 
 export const DashBoardContextProvider = ({ children }) => {
   const [selectedList, setSelectedList] = useState("hello ");
+  const [isActive, setIsActive] = useState("active");
 
   return (
-    <DashBoardContext.Provider value={{ selectedList, setSelectedList }}>
+    <DashBoardContext.Provider
+      value={{ selectedList, isActive, setIsActive, setSelectedList }}
+    >
       {children}
     </DashBoardContext.Provider>
   );
