235a6a84ad12f46f86c54daed9bcc2bb5a0bb244
Ebenezer <agyekumebenezer2001@gmail.com>
2023-06-11 14:27:57 +0000

    last forms for landlord

---
+++ gamkrib/src/components/admin/landlord/subPages/propertyListing/FacilityAmenities.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { DashboardContainer } from "../../pages/Listing";
+
+export const FacilityAmenities = () => {
+  return <DashboardContainer></DashboardContainer>;
+};
