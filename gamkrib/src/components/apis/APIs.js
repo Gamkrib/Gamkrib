e9b9a5ffbb19ba179de1134e0219815007e0d9dc
Ebenezer Agyekum <agyekumebenezer2001@gmail.com>
2023-08-05 23:00:35 +0000

    🙌

--- gamkrib/src/components/apis/APIs.js
+++ gamkrib/src/components/apis/APIs.js
@@ -12,14 +12,14 @@ const csrfToken = 'UdyHUorwRirpQv36bPC3SC78ScSfuneHJqhVB90YOF7Q2D0QPl0GsGo1o98oj
 
 export const base = async (route) => {
     try {
-        const resp = await axios(`${apiUrl}${route}/`,
+        const { data } = await axios(`${apiUrl}${route}/`,
             {
                 headers: {
                     'accept': 'application/json',
                     'X-CSRFToken': csrfToken,
                 }
             })
-        return console.log(resp)
+        return data
     } catch (error) {
         return console.log(error)
     }
